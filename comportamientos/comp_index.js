c3c6c39f5d6687e55c70f06257f200d51188f399
RoxxVS <roxanavaleriasolernou@gmail.com>
2023-01-15 23:35:20 -0300

    agregados los comportamientos y mas

---
+++ comportamientos/comp_index.js
@@ -0,0 +1,18 @@
+function irAWsp() {
+    location.href ='https://web.whatsapp.com/';
+}
+function irAFb() {
+    location.href ='https://es-la.facebook.com';
+}
+function irATt() {
+    location.href ='https://twitter.com';
+}
+function irAIg() {
+    location.href ='https://www.instagram.com';
+}
+function irAGh() {
+    location.href ='https://github.com';
+}
+function irAlMenu() {
+    location.href ='index.html';
+}
